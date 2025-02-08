59648585dc8fa141313c41e5a767124a8bf117d4
Jasmitha Garre <jasmithagar@gmail.com>
2025-02-07 20:56:08 -0600

    Initial Commit

---
+++ CoffePunchCard/CoffePunchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffePunchCardApp.swift
+//  CoffePunchCard
+//
+//  Created by Jasmitha G on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffePunchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffePunchCard/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CoffePunchCard
+//
+//  Created by Jasmitha G on 2/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
