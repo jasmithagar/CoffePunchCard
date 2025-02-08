579e27c98cc3fcdd8451308f57ad03f727512709
Neha Kamat <nehakamat81@gmail.com>
2025-02-08 12:12:29 -0600

    loginpagecommit1

---
+++ CoffePunchCard/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  CoffePunchCard
+//
+//  Created by Neha Kamat on 2/8/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
