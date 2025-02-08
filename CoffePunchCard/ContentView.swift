807e2085b3b4d532fe8c10fcf2696afdcfb945b6
Jasmitha Garre <jasmithagar@gmail.com>
2025-02-07 22:07:52 -0600

    1,2,3

--- CoffePunchCard/ContentView.swift
+++ CoffePunchCard/ContentView.swift
@@ -14,10 +14,26 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            VStack(alignment: .leading) {
+                Text("1")
+            }
+            // aligns views to the left (L to R)
+
+            VStack(alignment: .center) {
+                Text("2")
+            }
+            // aligns views in the middle (default)
+
+            VStack(alignment: .trailing) {
+                Text("3")
+            }
+            // aligns views to the right (R to L)
         }
         .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
