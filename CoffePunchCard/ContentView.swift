94997f5567ada57e9ba62c00f81423528a4c44e7
Jasmitha Garre <jasmithagar@gmail.com>
2025-02-08 09:44:38 -0600

    Store Buttons Work!

--- CoffePunchCard/ContentView.swift
+++ CoffePunchCard/ContentView.swift
@@ -13,7 +13,10 @@ struct ContentView: View {
             "Dunkin' Donuts",
             "Blue Bottle Coffee",
             "Peet's Coffee",
-            "Tim Hortons"
+            "Tim Hortons",
+            "abc"
+            
+        
         ]
         
         var body: some View {
@@ -22,19 +25,29 @@ struct ContentView: View {
 //                    Color.offWhite
                     
                     List(coffeeShops, id: \ .self) { shop in
+//                        NavigationLink(destination: GroundUpView()){
+//                            Text("Ground Up")
+//                        }
                         ZStack(alignment: .center) {
+                            
                             RoundedRectangle(cornerRadius: 25)
-                                .fill(Color.blue)
+                                .fill(Color.pink)
                                 .frame(width: UIScreen.screenWidth * 0.85, height: UIScreen.screenHeight * 0.13, alignment: .center)
 
                                 .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
                                 .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
-                            Text(shop)
+//                            Text(shop)
+                            NavigationLink(destination: GroundUpView()){
+                                Text(shop)
+                                    .frame(alignment: .center)
+                                    .multilineTextAlignment(.center)
+                            }
                                 .font(.largeTitle)
                                 .fontWeight(.semibold)
                                 .foregroundColor(Color.white)
                                 .multilineTextAlignment(.center)
                         }
+                        
                     }
                 }
                 .navigationBarTitle(Text("Coffee Shops"))
